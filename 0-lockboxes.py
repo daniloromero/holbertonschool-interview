8db6430e17d79df21303080f0cdc0246015d6765
daniloromero <1601@holbertonschool.com>
2021-04-19 12:41:34 +0000

    first commit

---
+++ 0-lockboxes.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/python3
+""" method that determines if all the boxes can be opened """
+
+
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened"""
+    return True
