73a11b69f2ab4d4f6fd841bd6079ee2bff84a46d
daniloromero <1601@holbertonschool.com>
2021-06-23 11:32:18 +0000

    Update Refactor from decimal to binary and bitwise

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,17 +8,17 @@ def validUTF8(data):
     """method that determines if a given data set represents
     a valid UTF-8 encoding
     """
-    count = 0
+    byte_size = 0
     for byte in data:
-        if count == 0:
-            if byte >= 192 and byte < 224:
-                count += 1
-            if byte >= 224 and byte < 240:
-                count += 2
-            if byte > 240:
-                count += 3
+        if byte_size == 0:
+            if byte >> 5 == 0b110:
+                byte_size += 1
+            if byte >> 4 == 0b1110:
+                byte_size += 2
+            if byte >> 3 == 0b11110:
+                byte_size += 3
         else:
-            if byte < 128:
+            if byte >> 6 != 0b10:
                 return False
-            count -= 1
+            byte_size -= 1
     return True
