6316c4a01c8279d96f07344c028a21581f3c5be7
daniloromero <1601@holbertonschool.com>
2021-06-23 06:01:01 +0000

    First commit

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module that determines if a given data set represents a valid UTF-8 encodin
+"""
+
+
+def validUTF8(data):
+    """method that determines if a given data set represents
+    a valid UTF-8 encoding
+    """
+    count = 0
+    for byte in data:
+        if count == 0:
+            if byte > 127 and byte < 192:
+                count += 1
+            if byte > 192 and byte < 224:
+                count += 2
+            if byte > 224 and byte < 240:
+                count += 3
+        else:
+            if byte < 128:
+                return False
+    return True
+
---
+++ 0x09-utf8_validation/README.md
@@ -0,0 +1,13 @@
+# 0x09. UTF-8 Validation
+
+
+method that determines if a given data set represents a valid UTF-8 encoding.
+
+Return: True if data is a valid UTF-8 encoding, else return False
+
+```
+usr@ubuntu$ ./0-main.py
+True
+True
+False
+```
