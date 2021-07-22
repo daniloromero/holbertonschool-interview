382b681d2fd25796ca6193cd49f2c9935ae8f72e
daniloromero <1601@holbertonschool.com>
2021-07-22 23:16:58 +0000

    first commit

---
+++ 0x0C-queens/0-nqueens.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""N queens puzzle is the challenge of placing N non-attacking queens
+on an N×N chessboard
+"""
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
