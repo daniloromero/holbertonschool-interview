e2e09fd469eb9c9e83a5f647a9ad728cbcd5a911
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-08-19 22:52:50 -0500

    First commit

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Given a list of non-negative integers representing the heights of walls with
+unit width 1, as if viewing the cross-section of a relief map, calculate how
+many square units of water will be retained after it rains.
+"""
+
+
+def rain(walls):
+    """calculate how many square units of water will be retained after it rains
+    Args:
+        walls: a list of non-negative integers
+    Return: integer indicating total amount of rainwater retained.
+            If the list is empty return 0.
+    """
+    if type(walls) != list or len(walls) < 3:
+        return 0
+    if len([*filter(lambda x: x < 0, walls)]) > 0:
+        return 0
+    l_height = 0
+    r_height = 0
+    width = 0
+    water = 0
+    for i in range(len(walls)):
+        if l_height == 0 and walls[i] > 0:
+            l_height = walls[i]
+        if l_height and walls[i] != 0:
+            r_height = walls[i]
+        if walls[i] == 0:
+            width += 1
+        if walls[i] != 0 and r_height:
+            water += min(l_height, r_height) * width
+            width = 0
+            l_height = r_height
+    return water
