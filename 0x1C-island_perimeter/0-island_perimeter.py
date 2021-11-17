7b5812438b9e51d47c2fae1e94c068bd3f4dfea7
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-11-17 15:59:00 -0500

    First commit

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """
+    Args:
+        grid is a list of list of integers:
+            0 represents water
+            1 represents land
+            Each cell is square, with a side length of 1
+            Cells are connected horizontally/vertically (not diagonally).
+            grid is rectangular, with its width and height not exceeding 100
+    Return: integer indicating the erimeter of the island
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+
+    count = 0
+    for r in range(1, rows - 1):
+        for c in range(1, cols - 1):
+            if grid[r][c] == 1:
+                if grid[r - 1][c] == 0:
+                    count += 1
+                if grid[r + 1][c] == 0:
+                    count += 1
+                if grid[r][c - 1] == 0:
+                    count += 1
+                if grid[r][c + 1] == 0:
+                    count += 1
+
+
+    return count
