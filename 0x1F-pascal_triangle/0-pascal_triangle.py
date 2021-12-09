6842cc7f65f56333fbfabf6d6e14e0dc6ef1c102
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-12-09 12:20:33 -0500

    Update 755 permission to files

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -13,11 +13,11 @@ def pascal_triangle(n):
         return triangle
     row = []
     for i in range(1, n + 1):
-        # set edges of triangle wit 1
+        # set edges of triangle with 1
         for j in range(1, i + 1):
             if j == 1 or j == len(triangle) + 1:
                 row.append(1)
-            # Calculate the inner indexex of the triangle
+            # Calculate the inner indexes of the triangle
             else:
                 row.append(triangle[i - 2][j - 2] + triangle[i - 2][j - 1])
         triangle.append(row)
